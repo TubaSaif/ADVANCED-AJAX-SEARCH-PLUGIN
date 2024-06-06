dd39ffaaed648e65a10106da83de115ff4151c6e
Tuba <tubasaif718@gmail.com>
2024-06-06 18:18:31 +0500

    update setting page

--- clinc/classes/aasp_settings_api.php
+++ clinc/classes/aasp_settings_api.php
@@ -15,7 +15,7 @@ if (!class_exists('AASP_SettingAPI')):
         private $settings_api;
 
         function __construct() {
-            $this->aasp_settingapi = new WeDevs_Settings_API;
+            $this->settings_api = new WeDevs_Settings_API;
 
             add_action('admin_init', array($this, 'admin_init'));
             add_action('admin_menu', array($this, 'admin_menu'));
@@ -62,27 +62,172 @@ if (!class_exists('AASP_SettingAPI')):
             
             $settings_fields = array(
                 'aasp_search_form' => array(
-                    array('name' => 'search_value'),
-                    array('name' => 'search_btn'),
-                    array('name' => 'search_bar_width'),
-                    array('name' => 'search_bar_height'),
-                    array('name' => 'action_charaters'),
-                    array('name' => 'show_loader'),
-                    array('name' => 'search_action'),
-                    array('name' => 'search_form_style'),
-                    array('name' => 'how_to_use'),
+                array(
+                'name' => 'text',
+                'label' => __('Text Input', 'wedevs'),
+                'desc' => __('Text input description', 'wedevs'),
+                'type' => 'text',
+                'default' => 'Title'
+                ),
+                array(
+                'name' => 'textarea',
+                'label' => __('Textarea Input', 'wedevs'),
+                'desc' => __('Textarea description', 'wedevs'),
+                'type' => 'textarea'
+                ),
+                array(
+                'name' => 'checkbox',
+                'label' => __('Checkbox', 'wedevs'),
+                'desc' => __('Checkbox Label', 'wedevs'),
+                'type' => 'checkbox'
+                ),
+                array(
+                'name' => 'radio',
+                'label' => __('Radio Button', 'wedevs'),
+                'desc' => __('A radio button', 'wedevs'),
+                'type' => 'radio',
+                'options' => array(
+                'yes' => 'Yes',
+                'no' => 'No'
+                )
+                ),
+                array(
+                'name' => 'multicheck',
+                'label' => __('Multile checkbox', 'wedevs'),
+                'desc' => __('Multi checkbox description', 'wedevs'),
+                'type' => 'multicheck',
+                'options' => array(
+                'one' => 'One',
+                'two' => 'Two',
+                'three' => 'Three',
+                'four' => 'Four'
+                )
+                ),
+                array(
+                'name' => 'selectbox',
+                'label' => __('A Dropdown', 'wedevs'),
+                'desc' => __('Dropdown description', 'wedevs'),
+                'type' => 'select',
+                'default' => 'no',
+                'options' => array(
+                'yes' => 'Yes',
+                'no' => 'No'
+                )
+                )
                 ),
                 'aasp_search_result' => array(
-                    array('name' => 'how_to_use'),
+                array(
+                'name' => 'text',
+                'label' => __('Text Input', 'wedevs'),
+                'desc' => __('Text input description', 'wedevs'),
+                'type' => 'text',
+                'default' => 'Title'
+                ),
+                array(
+                'name' => 'textarea',
+                'label' => __('Textarea Input', 'wedevs'),
+                'desc' => __('Textarea description', 'wedevs'),
+                'type' => 'textarea'
+                ),
+                array(
+                'name' => 'checkbox',
+                'label' => __('Checkbox', 'wedevs'),
+                'desc' => __('Checkbox Label', 'wedevs'),
+                'type' => 'checkbox'
+                ),
+                array(
+                'name' => 'radio',
+                'label' => __('Radio Button', 'wedevs'),
+                'desc' => __('A radio button', 'wedevs'),
+                'type' => 'radio',
+                'default' => 'no',
+                'options' => array(
+                'yes' => 'Yes',
+                'no' => 'No'
+                )
+                ),
+                array(
+                'name' => 'multicheck',
+                'label' => __('Multile checkbox', 'wedevs'),
+                'desc' => __('Multi checkbox description', 'wedevs'),
+                'type' => 'multicheck',
+                'default' => array('one' => 'one', 'four' => 'four'),
+                'options' => array(
+                'one' => 'One',
+                'two' => 'Two',
+                'three' => 'Three',
+                'four' => 'Four'
+                )
+                ),
+                array(
+                'name' => 'selectbox',
+                'label' => __('A Dropdown', 'wedevs'),
+                'desc' => __('Dropdown description', 'wedevs'),
+                'type' => 'select',
+                'options' => array(
+                'yes' => 'Yes',
+                'no' => 'No'
+                )
+                )
                 ),
                 'aasp_color' => array(
-                    array('name' => 'how_to_use'),
+                array(
+                'name' => 'text',
+                'label' => __('Text Input', 'wedevs'),
+                'desc' => __('Text input description', 'wedevs'),
+                'type' => 'text',
+                'default' => 'Title'
                 ),
-            );
+                array(
+                'name' => 'textarea',
+                'label' => __('Textarea Input', 'wedevs'),
+                'desc' => __('Textarea description', 'wedevs'),
+                'type' => 'textarea'
+                ),
+                array(
+                'name' => 'checkbox',
+                'label' => __('Checkbox', 'wedevs'),
+                'desc' => __('Checkbox Label', 'wedevs'),
+                'type' => 'checkbox'
+                ),
+                array(
+                'name' => 'radio',
+                'label' => __('Radio Button', 'wedevs'),
+                'desc' => __('A radio button', 'wedevs'),
+                'type' => 'radio',
+                'options' => array(
+                'yes' => 'Yes',
+                'no' => 'No'
+                )
+                ),
+                array(
+                'name' => 'multicheck',
+                'label' => __('Multile checkbox', 'wedevs'),
+                'desc' => __('Multi checkbox description', 'wedevs'),
+                'type' => 'multicheck',
+                'options' => array(
+                'one' => 'One',
+                'two' => 'Two',
+                'three' => 'Three',
+                'four' => 'Four'
+                )
+                ),
+                array(
+                'name' => 'selectbox',
+                'label' => __('A Dropdown', 'wedevs'),
+                'desc' => __('Dropdown description', 'wedevs'),
+                'type' => 'select',
+                'options' => array(
+                'yes' => 'Yes',
+                'no' => 'No'
+                )
+                )
+                )
+                );
             return $settings_fields;
         }
 
-        function plugin_page() {
+        function plugin_setting_page() {
             echo '<div class="wrap">';
 
             $this->settings_api->show_navigation();
